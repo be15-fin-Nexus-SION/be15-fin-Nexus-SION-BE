e0740a4ce43a65554f114900c6fa5de58b4da998
sezeme <stellaa223@gmail.com>
2025-06-17 17:33:08 +0900

    feat: add activeprofiles

--- src/test/java/com/nexus/sion/ProfileCheckTest.java
+++ src/test/java/com/nexus/sion/ProfileCheckTest.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class ProfileCheckTest {
 
     @Autowired
--- src/test/java/com/nexus/sion/SionApplicationTests.java
+++ src/test/java/com/nexus/sion/SionApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SionApplicationTests {
 
   @Test
