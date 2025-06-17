d7e83338f1ef78c113ca80a95c54217ab6947fa2
sezeme <stellaa223@gmail.com>
2025-06-17 17:20:14 +0900

    feat: add security config

--- src/test/java/com/nexus/sion/ProfileCheckTest.java
+++ src/test/java/com/nexus/sion/ProfileCheckTest.java
@@ -1,13 +1,16 @@
 package com.nexus.sion;
 
+import com.nexus.sion.config.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Import(SecurityConfig.class) // 테스트용 SecurityConfig 임포트
 public class ProfileCheckTest {
 
     @Autowired
--- src/test/java/com/nexus/sion/SionApplicationTests.java
+++ src/test/java/com/nexus/sion/SionApplicationTests.java
@@ -1,9 +1,12 @@
 package com.nexus.sion;
 
+import com.nexus.sion.config.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
+@Import(SecurityConfig.class) // 테스트용 SecurityConfig 임포트
 @SpringBootTest
 class SionApplicationTests {
 
