b49c53f2c6f2581eb505e015c29f5592236f7374
sezeme <stellaa223@gmail.com>
2025-07-06 20:23:01 +0900

    feat: 알림 엔터티 생성

---
+++ src/main/java/com/nexus/sion/feature/notification/command/domain/aggregate/Notification.java
@@ -0,0 +1,49 @@
+package com.nexus.sion.feature.notification.command.domain.aggregate;
+
+import jakarta.persistence.*;
+
+import com.nexus.sion.common.domain.BaseTimeEntity;
+
+import lombok.*;
+
+@Entity
+@Table(name = "notification")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+@ToString
+public class Notification extends BaseTimeEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "notification_id")
+  private Long notificationId;
+
+  @Column(name = "notification_type", length = 50, nullable = false)
+  private NotificationType notificationType;
+
+  @Column(name = "linked_content_id", length = 30)
+  private String linkedContentId;
+
+  @Column(name = "message", columnDefinition = "TEXT", nullable = false)
+  private String message;
+
+  @Column(name = "is_read", nullable = false)
+  private Boolean isRead = false;
+
+  @Column(name = "receiver_id", length = 30, nullable = false)
+  private String receiverId;
+
+  /**
+   * 테스트용 팩토리 메서드
+   */
+  public static Notification of(NotificationType notificationType, String message, String receiverId) {
+    return Notification.builder()
+            .notificationType(notificationType)
+            .message(message)
+            .receiverId(receiverId)
+            .isRead(false)
+            .build();
+  }
+}
